0c8b69d084189146207b7e06b76b71163edc3e11
peterlaxalt <pete@laxaltandmciver.co>
2016-08-25 13:09:15 -0700

    Establishing git & uploading files

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php include('components/header.html') ?>
+<?php include('components/search.html') ?>
+<?php include('components/footer.html') ?>
---
+++ results.php
@@ -0,0 +1,3 @@
+<?php include('components/header.html') ?>
+<?php include('components/result-table.html') ?>
+<?php include('components/footer.html') ?>
